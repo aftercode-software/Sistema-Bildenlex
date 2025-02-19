1a88a71c0fb34c143dbe911dcaa4aa4651bb44f0
Gabriel Pérez dIEZ <gaabgames@gmail.com>
2025-02-19 12:29:51 -0300

    cambios

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="flex-1">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
